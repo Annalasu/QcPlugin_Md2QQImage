d7b92bc8d8f60a5c5dfd8784d5bff4cff6242c2a
Annalasu <goolge2964@gmail.com>
2024-08-05 20:44:56 +0800

    创建插件v0.1

--- main.py
+++ main.py
@@ -1,49 +1,46 @@
 from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
-from pkg.plugin.events import *  # 导入事件类
+from pkg.plugin.events import PersonNormalMessageReceived, GroupNormalMessageReceived
+import re
+import mirai
 
 
-# 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class MyPlugin(BasePlugin):
+@register(name="Md2QQImage", description="Convert Markdown images to QQ images", version="0.1",
+          author="Annalasu")
+class MarkdownImagePlugin(BasePlugin):
 
-    # 插件加载时触发
     def __init__(self, host: APIHost):
-        pass
+        self.host = host
 
-    # 异步初始化
     async def initialize(self):
         pass
 
-    # 当收到个人消息时触发
     @handler(PersonNormalMessageReceived)
     async def person_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
-
-            # 阻止该事件默认行为（向接口获取回复）
+        msg = ctx.event.text_message
+        converted_message = self.convert_markdown_to_qq_message(msg)
+        if converted_message:
+            await ctx.reply(converted_message)
             ctx.prevent_default()
 
-    # 当收到群消息时触发
     @handler(GroupNormalMessageReceived)
     async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
+        msg = ctx.event.text_message
+        converted_message = self.convert_markdown_to_qq_message(msg)
+        if converted_message:
+            await ctx.reply(converted_message)
             ctx.prevent_default()
 
-    # 插件卸载时触发
+    def convert_markdown_to_qq_message(self, markdown_message: str):
+        markdown_image_pattern = r'!\[.*?\]\((.*?)\)'
+        image_urls = re.findall(markdown_image_pattern, markdown_message)
+        if not image_urls:
+            return None
+
+        qq_message_chain = mirai.MessageChain()
+        for url in image_urls:
+            qq_message_chain.append(mirai.Image.from_url(url))
+
+        return qq_message_chain
+
     def __del__(self):
         pass
